b7679d4504c479553aded9feb488f537045e2d9b
Loren Norman <lorennorman@gmail.com>
2010-01-14 22:08:25 -0500

    3 maps and first teleport triggers.

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,24 @@
-function game() {
+RequireSystemScript("screen.js");
+
+function game() 
+{
 	CreatePerson("Circle Man", "circle.rss", false);
 	AttachInput("Circle Man");
 	AttachCamera("Circle Man");
-	MapEngine("orange_map.rmp", 60);
+	MapEngine("valley_region.rmp", 60);
+}
+
+function change_map(map_name)
+{
+	FadeOut(700);
+	Rectangle(0,0,GetScreenWidth(), GetScreenHeight(), Black);
+	ChangeMap(map_name+".rmp");
+	UpdateMapEngine();
+	RenderMap();
+	FadeIn(700);
+}
+
+function consume_keys()
+{
+	while(AreKeysLeft()) { GetKey(); }
 }
