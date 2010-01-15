f761c6a112aa20a748d90d827af8b1c73004b9c9
Loren Norman <lorennorman@gmail.com>
2010-01-14 22:55:28 -0500

    Improved teleport function, monkey-patched Number for pixel-to-grid calculation.

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,31 @@
 RequireSystemScript("screen.js");
 
+Number.prototype.grids = function() {
+	return this * 17;
+};
+
+var PLAYER = "Circle Man";
+
+
+
 function game() 
 {
-	CreatePerson("Circle Man", "circle.rss", false);
-	AttachInput("Circle Man");
-	AttachCamera("Circle Man");
-	MapEngine("valley_region.rmp", 60);
+	CreatePerson(PLAYER, "circle.rss", false);
+	AttachInput(PLAYER);
+	AttachCamera(PLAYER);
+	MapEngine("valley_village.rmp", 60);
 }
 
-function change_map(map_name)
+function change_map(map_name, x, y)
 {
 	FadeOut(700);
 	Rectangle(0,0,GetScreenWidth(), GetScreenHeight(), Black);
 	ChangeMap(map_name+".rmp");
+	if(x && y)
+	{
+	  SetPersonX(PLAYER, x.grids());
+	  SetPersonY(PLAYER, y.grids());
+	}
 	UpdateMapEngine();
 	RenderMap();
 	FadeIn(700);
